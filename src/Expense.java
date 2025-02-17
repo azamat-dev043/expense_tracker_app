ac7eb4a1d5f1340c35556b2469e3a5e9c7eba1ec
azamat-dev043 <azamat_duisenbekov@icloud.com>
2025-02-18 00:26:43 +0500

    solved

---
+++ src/Expense.java
@@ -0,0 +1,14 @@
+public class Expense {
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        System.out.println("Program started!");
+    }
+
+
+    public Expense() {
+        System.out.println("Hello!");
+    }
+}
